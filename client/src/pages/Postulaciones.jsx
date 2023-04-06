9a06101434c80add85999e36f7b397c5e586127a
Devzamora99 <cristianHernandezZ1999@gmail.com>
2023-04-06 13:48:05 -0600

    Avances de las postulaciones que tiene un usuario

--- client/src/pages/Postulaciones.jsx
+++ client/src/pages/Postulaciones.jsx
@@ -1,13 +1,82 @@
 import { Header } from "../components/Header";
-import { BaseContainer, BaseBody, BaseSection} from "../styles/base";
+import { TarjetaEmpleo } from "../components/empleo";
+import { BaseContainer, BaseBody, BaseSection, SectionContainer} from "../styles/base";
+
+const empleos = [
+    {
+        titulo: "Programador C#",
+        empresa: "Oracle",
+        descripcionEmpleo: "Programador con experiencia"
+    },
+    {
+        titulo: "Programador Java",
+        empresa: "DevJb",
+        descripcionEmpleo: "Programador con experiencia"
+    },
+    {
+        titulo: "Programador C#",
+        empresa: "Oracle",
+        descripcionEmpleo: "Programador con experiencia"
+    },
+    {
+        titulo: "Programador Java",
+        empresa: "DevJb",
+        descripcionEmpleo: "Programador con experiencia"
+    },
+]
+
+    const getItems = empleos.map((item, key)=>{
+        
+    })
+
 
 export const Postulaciones = () => {
     return (
         <BaseContainer>
             <Header titulo="Postulaciones"/>
             <BaseBody>
+                <h1>Postulaciones realizadas</h1>
                 <BaseSection>
-                    Contenido
+                    <SectionContainer>
+                        <TarjetaEmpleo 
+                            tituloEmpleo = "Titulo del empleo" 
+                            nombreEmpresa="Nombre de la empresa" 
+                            descripcionEmpleo="Descripcion del empleo"
+                            enlace="#">
+                        </TarjetaEmpleo>
+                        <TarjetaEmpleo 
+                            tituloEmpleo = "Titulo del empleo" 
+                            nombreEmpresa="Nombre de la empresa" 
+                            descripcionEmpleo="Descripcion del empleo"
+                            enlace="#">
+                        </TarjetaEmpleo>
+                        <TarjetaEmpleo 
+                            tituloEmpleo = "Titulo del empleo" 
+                            nombreEmpresa="Nombre de la empresa" 
+                            descripcionEmpleo="Descripcion del empleo"
+                            enlace="#">
+                        </TarjetaEmpleo>
+                    </SectionContainer>
+                    <SectionContainer>
+                        <TarjetaEmpleo 
+                            tituloEmpleo = "Titulo del empleo" 
+                            nombreEmpresa="Nombre de la empresa" 
+                            descripcionEmpleo="Descripcion del empleo"
+                            enlace="#">
+                        </TarjetaEmpleo>
+                        <TarjetaEmpleo 
+                            tituloEmpleo = "Titulo del empleo" 
+                            nombreEmpresa="Nombre de la empresa" 
+                            descripcionEmpleo="Descripcion del empleo"
+                            enlace="#">
+                        </TarjetaEmpleo>
+                        <TarjetaEmpleo 
+                            tituloEmpleo = "Titulo del empleo" 
+                            nombreEmpresa="Nombre de la empresa" 
+                            descripcionEmpleo="Descripcion del empleo"
+                            enlace="#">
+                        </TarjetaEmpleo>
+                    </SectionContainer>
                 </BaseSection>
             </BaseBody>
         </BaseContainer>

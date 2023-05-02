b481c8016818a579b469824e062dc67ae5ec3a90
Devzamora99 <cristianHernandezZ1999@gmail.com>
2023-05-01 18:36:46 -0600

    Obtencion de roles desde el login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,32 @@
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Header } from "../components/Header";
 import { BaseContainer, BaseBody, BaseSection } 
 from "../styles/base";
-import "../styles/pages/login.css"
+import "../styles/pages/login.css";
+import { useCustomFetch } from "../hooks/useCustomFetch";
+
 
 export const Login = () => {
+    let url = "http://localhost:3000/api/roles"
+
+    const { pathname } = useLocation()
+
+    let {
+        dataBase,
+        dataToEdit,
+        setDataToEdit,
+        createData,
+        updateData,
+        deleteData,
+        error,
+        loading,
+        response,
+        setResponse,
+        handleClick
+    } = useCustomFetch(url);
+    
+    //const roles = dataBase.rols;
+    //console.log(roles);
     return (
         <BaseContainer>
             <Header titulo="Login"/>
@@ -16,6 +39,12 @@ export const Login = () => {
                                 <label htmlFor="chk" aria-hidden="true">Crear Cuenta</label>
                                 <input type="text" name="txt" placeholder="Nombre de usuario" required=""/>
                                 <input type="email" name="email" placeholder="Email" required=""/>
+                                <select onChange={event => console.log(event.target.value)}>
+                                    <option value="">Seleccione un rol</option>
+                                    
+                                    <option value="1" >1</option>
+                                    <option value="2">2</option>
+                                </select>
                                 <input type="password" name="pswd" placeholder="Contraseña" required=""/>
                                 <button>Crear Cuenta</button>
                             </form>
